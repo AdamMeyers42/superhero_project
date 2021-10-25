dcfc7ccce2a818faf3d975189ce9e675ba2fea2f
AdamMeyers42 <adammeyers3551@gmail.com>
2021-10-24 21:58:21 -0700

    Completed edit and delete functions in view.py, created the paths in urls.py, updated indext.html to reflect the updated paths.

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('<int:hero_id>/', views.detail, name = 'detail')
+    path('<int:hero_id>/', views.detail, name = 'detail'),
+    path('new/', views.create, name = 'create'),
+    path('edit/<int:hero_id>/', views.edit, name = 'edit'),
+    path('delete/<int:hero_id>/', views.delete, name = 'delete')
 ]
--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
+# from django.db.models import F
 from .models import Superhero
 
 # Create your views here.
@@ -16,3 +18,52 @@ def detail(request, hero_id):
         'single_hero': single_hero
     }
     return render(request, 'superheroes/detail.html', context)
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
+
+def edit(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    if single_hero is None:
+        return Http404
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        single_hero.name = name
+        single_hero.alter_ego = alter_ego
+        single_hero.primary_ability = primary
+        single_hero.secondary_ability = secondary
+        single_hero.catch_phrase = catchphrase
+        single_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+         context = {
+             'hero_id': hero_id
+         }
+         return render(request, 'superheroes/edit.html', context)
+
+def delete(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    if single_hero is None:
+        return Http404
+    single_hero.delete()
+    all_heroes = Superhero.objects.all()
+    context = {
+        'all_heroes': all_heroes
+    }
+    return render(request, 'superheroes/index.html', context)
+    
+
